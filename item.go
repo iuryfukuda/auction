69fbdfaff35a0ac53268ad25955d34a7a2a7643e
Iury Fukuda <iuryfukuda@gmail.com>
2019-08-15 01:03:02 -0300

    add initial structs

---
+++ item.go
@@ -0,0 +1,14 @@
+import (
+	"enconnding/json"
+)
+
+type Item struct {
+	// ID is the id of item
+	ID string	`json:"item_id"`
+
+	// Hits is used to count the hits made in item
+	Hits int	`json:"hits"`
+
+	// BestBid is used to store the best bid made so far
+	BestBid Bid	`json:"best_bid"`
+}
