381371d291ac4deb7dc9242e9d584f0f4f1b7a96
Iury Fukuda <iuryfukuda@gmail.com>
2019-08-16 16:25:09 -0300

    fix handlers package name

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"log"
 	"net/http"
 	"github.com/iuryfukuda/auction/app"
-	"github.com/iuryfukuda/auction/handles"
+	"github.com/iuryfukuda/auction/handlers"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	a := app.New()
-	mux.HandleFunc("/bid", handles.Bid)
-	// mux.HandleFunc("/stats", handles.Stats)
+	mux.HandleFunc("/bid", handlers.Bid)
+	// mux.HandleFunc("/stats", handlers.Stats)
 	log.Printf("a=(%+v)", a)
 	log.Fatal(http.ListenAndServe(":3000", mux))
 }
